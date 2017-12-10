e1e1c8481b76dc19d2958bdae9c626c4376a260c
Kerin Khan <kerink585@gmail.com>
2017-12-10 15:02:41 -0800

    changed css colors and added an API for random quotes

--- randomQuote.js
+++ randomQuote.js
@@ -1,59 +1,44 @@
+
+var myRequest= new XMLHttpRequest();
 function getRandomQuote(){
-    var theQuotes = [
-    {
-    	"Quote":"'We can only see a short distance ahead, but we can see plenty there that needs to be done.'",
-    	"name":"Alan Turing"
-    },
-    {
-    	"Quote":"'I propose to consider the question, 'Can machines think?'",
-    	"name":"Alan Turing"
-    },
-    {
-    	"Quote":"'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.'",
-    	"name":"Alan Turing"
-    },
-    {
-    	"Quote":"'It is better to be feared than loved, if you cannot be both.' ",
-    	"name":"Niccolo Machiavelli"
+    var myRequest= new XMLHttpRequest();
+    myRequest.onreadystatechange = function(){
+        if(myRequest.readyState === XMLHttpRequest.DONE){
+            if(myRequest.status < 400){
+                var str = JSON.parse(myRequest.responseText);
+                var quote = str.value;
+                quoteButton(quote);
 
-    },
-    {
-    	"Quote":"'Being entirely honest with oneself is a good exercise.'",
-    	"name":"Sigmund Freud"
-
-    },
-    {
-    	"Quote":"'There is only one happiness in this life, to love and be loved.'",
-    	"name":"George Sand"
-
-    },
-    {
-    	"Quote":"'Learning never exhausts the mind.'",
-    	"name":"Leonardo da Vinci"
-    },
-    {
-    	"Quote":"'Sometimes it's the very people who no one imagines anything of who do the things no one can imagine.'",
-    	"name":"Christopher Morcom"
-    }
-    ]
-    //use Math.random to store index number
-	indexNum = Math.floor(Math.random()*theQuotes.length);
-	var quote = theQuotes[indexNum];
-	return quote;
+                
+            }
+        }
+    };
+    var url = 'https://api.chucknorris.io/jokes/random';
+    myRequest.open('GET',url,true);
+    myRequest.send(null);
 }
 
 
 function tweetIt(){
-	var url = "https://twitter.com/intent/tweet";
-	var text = document.getElementById("message").innerHTML;
-	var hashtags ="hi,hello";
-	var via ="userName";
-	window.open(url+"?text="+text+";hashtags"+hashtags+";via"+via,"","width=500,height =300");
+    var url = "https://twitter.com/intent/tweet";
+    var text = document.getElementById("message").innerHTML;
+    var hashtags ="#";
+    var via ="userName";
+    window.open(url+"?text="+text+";hashtags"+hashtags+";via"+via,"","width=500,height =300");
 
 }
 
 
-function quoteButton(){
-	var quote = getRandomQuote();
-	document.getElementById("message").innerHTML = quote.Quote + '\n\r' + quote.name;
+function quoteButton(quote){
+    var color = ['#5E0D24','#75173A','#A1254C','#D4CFCA','#040408'];
+    var fontcolor = ['#D00E51','#007AAA','#FDB400','#FF9A00','#FF9A00'];
+    
+    var element= document.getElementById("message");
+    element.innerHTML= quote;
+    element.style.color=fontcolor[Math.floor(Math.random()*color.length)];;
+    var box = document.getElementById('box');
+    box.style.background=color[Math.floor(Math.random()*color.length)];
+    var innerbox = document.getElementsByClassName('well')[0];
+    innerbox.style.background=color[Math.floor(Math.random()*color.length)];
+
 }
